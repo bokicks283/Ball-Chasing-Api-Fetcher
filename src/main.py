4f4bac1861880922154519c8d58f171393e73efd
Ronald Bocchichio <rbocchichio@gmail.com>
2026-02-05 00:09:21 -0500

    Added some placeholders. More to come!

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+import requests
+import os
+from dotenv import load_dotenv
+
+def main():
+    load_dotenv("../.env")
+    token = os.environ.get("BC_API_TOKEN")
+    
+
+if __name__ == "__main__":
+    main()
